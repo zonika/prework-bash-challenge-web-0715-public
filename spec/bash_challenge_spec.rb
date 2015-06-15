bccaa4536ba9522c1f826296e7fde3a6492a9760
zonika <zchodosh@conncoll.edu>
2015-06-15 19:27:07 -0400

    all done

--- spec/bash_challenge_spec.rb
+++ spec/bash_challenge_spec.rb
@@ -1,29 +1,29 @@
 describe "Bash Challenge" do
   context 'Navigation' do
     it 'determines what the current working directory is' do
-      your_answer = __
+      your_answer = "pwd"
       expect(your_answer).to eq(answer_1) 
     end
 
     it 'changes directory up one level' do 
-      your_answer = __
+      your_answer = "cd .."
       expect(your_answer).to eq(answer_2)
     end
 
     it 'lists the contents of the working directory' do 
-      your_answer = __
+      your_answer = "ls"
       expect(your_answer).to eq(answer_3)
     end
 
     it 'brings us back to the home directory' do 
-      your_answer = __
+      your_answer = "cd ~"
       expect(your_answer).to satisfy {|s| answer_4.include?(s.downcase) }
     end
   end
 
   context 'Commands' do 
     it 'it creates a new filed called "text.html"' do
-      your_answer = __
+      your_answer = "touch text.html"
       expect(your_answer).to eq(answer_5)
     end
 
@@ -41,22 +41,22 @@ describe "Bash Challenge" do
       -rw-r--r--   1 janedoe  staff   1.2K Jan  5 12:42 README.md
       drwxr-xr-x   4 janedoe  staff   136B Jan  5 12:42 spec" do
 
-      your_answer = __
+      your_answer = "ls -lah"
       expect(your_answer).to eq(answer_6)
     end
 
     it 'opens a file called "readme.md" in its default format' do 
-      your_answer = __
+      your_answer = "open readme.md"
       expect(your_answer).to eq(answer_7)
     end
 
     it 'shows us what our path is' do 
-      your_answer = __
+      your_answer = "echo $PATH"
       expect(your_answer).to eq(answer_8)
     end
 
     it 'makes a new directory called "code"' do 
-      your_answer = __
+      your_answer = "mkdir code"
       expect(your_answer).to eq(answer_9)
     end
   end
